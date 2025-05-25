5d5b3280e686159f7d55950f204f016c648927b8
buibui699 <Michelewhjd@gmail.com>
2025-05-25 20:30:37 +0800

    Site updated: 2025-05-25 20:30:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/14/我的新文章/","2025/05/14/hello-world/"];function toRandomPost(){
+var posts=["2025/05/25/阅读笔记2/","2025/05/25/诗词1/","2025/05/14/hello-world/","2025/05/11/阅读笔记1/","2025/05/10/我的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
